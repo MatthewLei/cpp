5afabd51937f95f7e39e2a168ca4f9549e3d8135
Matthew L <lei.matthew93@gmail.com>
2016-08-12 18:53:20 -0700

    implemented png_check_sig()

--- png_util/png_util.cpp
+++ png_util/png_util.cpp
@@ -1,7 +1,24 @@
+//written by: Matthew Lei
+//png chunk specifications: https://www.w3.org/TR/PNG-Chunks.html
+//png file structure spec:  
+
 #include <iostream>
 #include <fstream>
 
 using namespace std;
+
+static const int SIG_SZ = 8;
+static const int PNG_SIG[SIG_SZ] = {137, 80, 78, 71, 13, 10, 26, 10};
+
+typedef struct Ihdr {
+    unsigned int width;      /// 4 bytes
+    unsigned int height;     /// 4 bytes
+    int8_t bit_depth;        /// 1 byte
+    int8_t color_type;       /// 1 byte
+    int8_t compression;      /// 1 byte
+    int8_t filter;           /// 1 byte
+    int8_t interlace;        /// 1 byte
+}Ihdr;
 
 /**
  *  check_input_str - checks for PNG extension
@@ -29,6 +46,67 @@ void get_file_name(string &fileName) {
     }
 }
 
+
+unsigned int read_4byte_uint(ifstream &fs) {
+    char b1, b2, b3, b4;
+    fs.get(b1);
+    fs.get(b2);
+    fs.get(b3);
+    fs.get(b4);
+    return (unsigned int) ((int) b1 + (int) b2 + (int) b3 + (int) b4);
+}
+
+/** @brief Checks for PNG file signiture
+ *
+ *  Checks first 8 bytes of the file for PNG
+ *  file signature according to PNG specifications.
+ *  sig (dec): 137, 80, 78, 71, 13, 10, 26, 10
+ *  sig (ascii): \211 P N G \r \n \032 \n
+ *  ref: http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
+ *
+ *  @param fs File stream
+ *  @return True if file has valid PNG signature
+ */
+bool png_check_sig(ifstream &fs) {
+    char byte;
+    unsigned char ubyte;
+    int cur;
+
+    cur = fs.tellg();
+    fs.seekg(fs.beg);
+    for (int i = 0; i < SIG_SZ; ++i) {
+	fs.get(byte);
+	ubyte = (unsigned char) byte;
+	if ((int) ubyte != PNG_SIG[i]) {
+	    cout << "file is not a PNG image or corrupted.\n";
+	    return false;
+	}
+    }
+    fs.seekg(cur);
+    return true;
+}
+
+Ihdr get_ihdr_chunk(ifstream &fs) {
+    Ihdr ihdr;
+    char a, b, c, d;
+    fs.get(a); cout << a;
+    fs.get(a); cout << a;
+    fs.get(a); cout << a;
+    fs.get(a); cout << a;
+    cout << read_4byte_uint(fs);
+    /*
+    fs.get(a);
+    fs.get(b);
+    fs.get(c);
+    fs.get(d);
+    cout << a << " " << b << " " << c << " " << d << endl;
+    cout << (int) a << " " << (int) b << " " << (int) c << " " << (int) d << endl;
+    */
+    return ihdr;
+}
+
+/***************************************************************************/
+
 int main(int argc, char** argv) {
     string fileName;
     ifstream file;
@@ -51,4 +129,6 @@ int main(int argc, char** argv) {
 	get_file_name(fileName);
 	file.open(fileName);
     }
+    png_check_sig(file);
+    //get_ihdr_chunk(file);
 }
